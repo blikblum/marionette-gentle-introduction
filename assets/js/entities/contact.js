simple-git-user/js_basic_marionette

handling data latency

assets/js/entities/contact.js
@@ -22,24 +22,43 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
     contacts.forEach(function(contact){
       contact.save();
     });
-    return contacts;
+    return contacts.models;
   };
 
   var API = {
     getContactEntities: function(){
       var contacts = new Entities.ContactCollection();
-      contacts.fetch();
-      if(contacts.length === 0){
-        // if we don't have any contacts yet, create some for convenience
-        return initializeContacts();
-      }
-      return contacts;
+      var defer = $.Deferred();
+      contacts.fetch({
+        success: function(data){
+          defer.resolve(data);
+        }
+      });
+      var promise = defer.promise();
+      $.when(promise).done(function(contacts){
+        if(contacts.length === 0){
+          // if we don't have any contacts yet, create some for convenience
+          var models = initializeContacts();
+          contacts.reset(models);
+        }
+      });
+      return promise;
     },
 
     getContactEntity: function(contactId){
       var contact = new Entities.Contact({id: contactId});
-      contact.fetch();
-      return contact;
+      var defer = $.Deferred();
+      setTimeout(function(){
+        contact.fetch({
+          success: function(data){
+            defer.resolve(data);
+          },
+          error: function(data){
+            defer.resolve(undefined);
+          }
+        });
+      }, 2000);
+      return defer.promise();
     }
   };
 

blikblum/marionette-gentle-introduction

assets/js/entities/contact.js
@@ -22,24 +22,43 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
     contacts.forEach(function(contact){
       contact.save();
     });
-    return contacts;
+    return contacts.models;
   };
 
   var API = {
     getContactEntities: function(){
       var contacts = new Entities.ContactCollection();
-      contacts.fetch();
-      if(contacts.length === 0){
-        // if we don't have any contacts yet, create some for convenience
-        return initializeContacts();
-      }
-      return contacts;
+      var defer = $.Deferred();
+      contacts.fetch({
+        success: function(data){
+          defer.resolve(data);
+        }
+      });
+      var promise = defer.promise();
+      $.when(promise).done(function(fetchedContacts){
+        if(fetchedContacts.length === 0){
+          // if we don't have any contacts yet, create some for convenience
+          var models = initializeContacts();
+          contacts.reset(models);
+        }
+      });
+      return promise;
     },
 
     getContactEntity: function(contactId){
       var contact = new Entities.Contact({id: contactId});
-      contact.fetch();
-      return contact;
+      var defer = $.Deferred();
+      setTimeout(function(){
+        contact.fetch({
+          success: function(data){
+            defer.resolve(data);
+          },
+          error: function(data){
+            defer.resolve(undefined);
+          }
+        });
+      }, 2000);
+      return defer.promise();
     }
   };
 